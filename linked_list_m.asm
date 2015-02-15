bd678de0ea6d84b23d0c0e9986cc8ce5f60004c3
SanaGhani <sana_pechian@yahoo.com>
2015-02-15 18:30:34 +0500

    create node modified

--- linked_list_m.asm
+++ linked_list_m.asm
@@ -22,27 +22,29 @@
 ##		$t1 - store the address where the node is created
 
 .data
-    	prompt:  .asciiz"Do you want to enter more data?\t\n"
+    prompt:  .asciiz"Do you want to enter more data?\t\n"
 	prompt1: .asciiz"Enter the data you want to add in the list:\t\n "
-    	prompt2: .asciiz"Press 1 for exit\n"
+    prompt2: .asciiz"Press 1 for exit\n"
 	linefeed:.asciiz"\n"
 	
 .text
 
 main:    
-#head node or sentinel node
-    # la $s2,address
+#mem allocation for 1st integer
+	 li $v0,9
+	 li $a0,4
+	 syscall
+	 move $t3,$v0
 	 
-	# li $v0,34
-	 #move $a0,$s2
-	 #syscall
+	 move $a0,$t3
+	 li $v0,1
+	 syscall
 	 
-     #add $s4,$s2,$0	 
-	 #li $s0,0     #sentinel value
-     #move $a0,$s0
+	 li $v0,4
+     la $a0,linefeed
+     syscall
 	 
 
-     #jal create_node #jump to create a node
 	 addi $t4,$0,0 # initializing counter i=0;
 	 addi $t5,$0,4 #size of loop
 loop:
@@ -50,13 +52,6 @@ loop:
      move $t1,$v0   #result of create node in t1
      addi $t4,$t4,1
 	 bne $t4,$t5,loop
-	 
-	 
-#print sentinel node	      
-#     li $v0,1
- #    lw $a0,0($t1)
-#	 syscall
-#print all the nodes in the list
 
 	
 	 
@@ -71,18 +66,17 @@ create_node:
      add $t0,$a0,$0
 #allocate 8 bytes for memory 
      li $v0,9
-     li $a0,8
+     li $a0,4
      syscall
 	 
+	 
      move $s1,$t0
-move $s2,$v0	
- sw $s1, ($s2)
-	 #addi $s2,$s2,4
-	addi $s3,$s2,4 
-	#sw  $0, ($s2)
-	sw $s3,4($s2)     	
-move $v0,$s2 
-	 #addi $s2,$s2,4
+     move $s2,$v0	
+     sw $s1, ($t3)
+	 sw $0,($s2) 	 
+	 move $t3,$s2	 
+     move $v0,$s2 
+
 	 jr $ra
 	
 #end                                                                                                                                                              of create_node 
@@ -96,7 +90,7 @@ add_node:
 
      add $t2,$ra,$0
      li $v0,5
-     syscall
+     syscall #Take Input
 
      move $a0,$v0
 	 jal create_node
